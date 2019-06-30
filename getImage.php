a349621fd936c2b513f40720a9ce2d941ef0d348
Akshay Sunil Pawar <31787030+pwraxe@users.noreply.github.com>
2019-06-30 16:56:44 +0530

    Create getImage.php

---
+++ getImage.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Only Photo</title>
+</head>
+<body>
+	<form action="getandset.php" method="post" enctype="multipart/form-data">
+		<input type="file" name="photo"><br><br>
+		<input type="submit" name="submit">
+	</form><br>
+	
+</body>
+</html>
